75d6cc2b65a9af76918691e3e97be4db54d58ec6
Unknown <mattliveuk@gmail.com>
2017-06-12 21:49:33 +0200

    Created table,acceleration , main methods , 2 player game mode

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Paddle2 = new System.Windows.Forms.PictureBox();
+            this.Paddle1 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Paddle2
+            // 
+            this.Paddle2.Image = ((System.Drawing.Image)(resources.GetObject("Paddle2.Image")));
+            this.Paddle2.Location = new System.Drawing.Point(12, 185);
+            this.Paddle2.Name = "Paddle2";
+            this.Paddle2.Size = new System.Drawing.Size(23, 202);
+            this.Paddle2.TabIndex = 0;
+            this.Paddle2.TabStop = false;
+            // 
+            // Paddle1
+            // 
+            this.Paddle1.Image = ((System.Drawing.Image)(resources.GetObject("Paddle1.Image")));
+            this.Paddle1.Location = new System.Drawing.Point(1305, 498);
+            this.Paddle1.Name = "Paddle1";
+            this.Paddle1.Size = new System.Drawing.Size(20, 202);
+            this.Paddle1.TabIndex = 1;
+            this.Paddle1.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(642, 271);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(21, 21);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1337, 700);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Paddle1);
+            this.Controls.Add(this.Paddle2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Paddle2;
+        private System.Windows.Forms.PictureBox Paddle1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const int movementspeed = 3, topWorld = 0, bottomWorld = 498; 
+        bool isUpPressedForPlayer1, isDownPressedForPlayer1, isUpPressedForPlayer2, isDownPressedForPlayer2;
+        bool? wasGoingUpLastTickForPlayer1, wasGoingUpLastTickForPlayer2;
+        int numberOfTickGoingInTheSameDirectionForPlayer1, numberOfTickGoingInTheSameDirectionForPlayer2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+            ProcessMove(Paddle1, isUpPressedForPlayer1, isDownPressedForPlayer1 , ref wasGoingUpLastTickForPlayer1 , ref numberOfTickGoingInTheSameDirectionForPlayer1);
+            ProcessMove(Paddle2, isUpPressedForPlayer2, isDownPressedForPlayer2, ref wasGoingUpLastTickForPlayer2 , ref numberOfTickGoingInTheSameDirectionForPlayer2);
+        }
+
+        private void ProcessMove(PictureBox paddle, bool isUpPressed, bool isDownPressed, ref bool? wasGoingUp, ref int numberOfTickGoingInTheSameDirection)
+        {
+            bool? goingUp = null;
+
+
+            if (isUpPressed)
+            {
+                goingUp = true;
+            }
+            if (isDownPressed)
+            {
+                if (goingUp.HasValue)
+                {
+                    goingUp = null;
+                }
+                else
+                {
+                    goingUp = false;
+                }
+            }
+
+            if(wasGoingUp.HasValue)
+            {
+                if(!goingUp.HasValue)
+                {
+                    wasGoingUp = null;
+                    numberOfTickGoingInTheSameDirection = 0;
+                } else if(wasGoingUp.Value == goingUp.Value)
+                {
+                    numberOfTickGoingInTheSameDirection++;
+                } else
+                {
+                    wasGoingUp = goingUp;
+                    numberOfTickGoingInTheSameDirection = 1;
+                }
+            } else if(goingUp.HasValue)
+            {
+                wasGoingUp = goingUp;
+                numberOfTickGoingInTheSameDirection = 1;
+            }
+
+
+
+            DoMove(paddle, goingUp , numberOfTickGoingInTheSameDirection);
+        }
+
+        private void DoMove(PictureBox paddle, bool? goingUp, int numberOfTickGoingInTheSameDirection)
+        {
+            if(goingUp.HasValue) {
+                var speed = movementspeed * (numberOfTickGoingInTheSameDirection / 10);
+                if(goingUp.Value)
+                {
+                    speed *= -1;
+                }
+                paddle.Location = new Point(paddle.Location.X,
+                   Math.Max(topWorld,
+                  Math.Min(bottomWorld, paddle.Location.Y + speed
+                   )
+                   ));
+            }
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            CheckKeys(e, true);
+
+
+
+            /*if(e.KeyCode == Keys.Up)
+            {
+                isUpPressed = true;
+            } else if (e.KeyCode == Keys.Down)
+            {
+                isDownPressed = true;
+            }*/
+
+
+        }
+
+        private void CheckKeys(KeyEventArgs e, bool isDown)
+        {
+            switch (e.KeyCode)
+            {
+               
+                case Keys.Up:
+                    isUpPressedForPlayer1 = isDown;
+                    break;
+                case Keys.Down:
+                    isDownPressedForPlayer1 = isDown;
+                    break;
+
+                case Keys.Oemcomma:
+                case Keys.W:
+                    isUpPressedForPlayer2 = isDown;
+                    break;
+                case Keys.O:
+                case Keys.S:
+                    isDownPressedForPlayer2 = isDown;
+                    break;
+
+
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            CheckKeys(e, false);
+
+
+            /*if (e.KeyCode == Keys.Up)
+            {
+                isUpPressed = false;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                isDownPressed = false;
+            }*/
+        }
+
+    }
+}
